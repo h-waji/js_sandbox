f4ff8422e09548bf6b4176af1e434b68f49c24eb
wajima <goo.hiro.w@gmail.com>
2023-03-04 20:09:58 +0900

    let, function

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,18 @@
-console.log('hello');
+console.log("hello world");
+
+// ----- let -----
+let number = 1;
+let string = "2";
+console.log(typeof number);
+console.log(typeof string);
+
+// ----- function -----
+function hello(name, age) {
+  console.log('Hello ' + name + ', age: ' + age);
+  return name + age;
+}
+
+const returnValue = hello('Hiro', 36);
+console.log(returnValue);
+
+
