fcee8acff686c0198b01d1e20c5113da2b2ae1ba
wajima <goo.hiro.w@gmail.com>
2023-03-11 11:22:21 +0900

    querySelector, querySelectorAll

---
+++ javascript/05_dom.js
@@ -0,0 +1,31 @@
+let element = document.querySelector("#main-title");
+console.log(element);
+
+element = document.getElementById("main-title"); // 古い書き方
+console.log(element);
+
+element = document.querySelector(".artcls");
+console.log(element);
+
+const h2 = document.querySelector("h2");
+console.log(h2);
+console.log(h2.textContent);
+
+const ul = document.querySelector("ul");
+ul.style.color = "red";
+
+const firstLi = ul.querySelector("li");
+firstLi.style.color = "black";
+
+// ----- querySelectorAll -----
+let items = document.querySelectorAll(".item");
+console.log(items);
+
+items = document.querySelectorAll('ul > li');
+console.log(items);
+
+const li = document.querySelectorAll('li');
+li[1].style.color = 'purple';
+
+items = document.querySelectorAll(".item2");
+items.forEach(node => node.style.color = 'blue');
