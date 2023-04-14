d64d6f449663020ddd5a2bc765c83281788e5c97
wajima <goo.hiro.w@gmail.com>
2023-04-14 21:02:21 +0900

    merge sort

---
+++ javascript/14_merge_sort.js
@@ -0,0 +1,38 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  // console.log(`left: ${left}`);
+  // console.log(`right: ${right}`);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  // console.log(`-- left: ${left}`);
+  // console.log(`-- right: ${right}`);
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+
+  return result.concat(left.slice(i)).concat(right.slice(j));
+}
+
+const arr = [9, 2, 7, 4, 5, 6, 3, 8, 1];
+console.log(mergeSort(arr));
